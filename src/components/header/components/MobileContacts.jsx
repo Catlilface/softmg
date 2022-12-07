4feece77fa8a1f20a61e3900a076a5cf461577d1
Catlilface <aleklim98@gmail.com>
2022-12-07 16:08:07 +0300

    updated contacts

--- src/components/header/components/MobileContacts.jsx
+++ src/components/header/components/MobileContacts.jsx
@@ -23,22 +23,34 @@ const MobileContacts = (props) => {
     <div style={{ right: props.margin }} className="mobile-contacts">
       <span>Связаться с нами</span>
       <div className="communicate">
-        <a href="" className="option">
-          <img src="./src/assets/bobile-phone.svg" />
-          <span>Позвонить</span>
-        </a>
-        <a className="option">
-          <img src="./src/assets/bobile-write.svg" />
-          <span>Написать</span>
-        </a>
-        <a className="option">
-          <img src="./src/assets/bobile-whatsapp.svg" />
-          <span>WhatsApp</span>
-        </a>
-        <a className="option">
-          <img src="./src/assets/bobile-telega.svg" />
-          <span>Написать</span>
-        </a>
+        {
+          options.phone.show &&
+            <a href={ 'tel:' + options.phone.link } className="option">
+              <img src="./src/assets/bobile-phone.svg" />
+              <span>Позвонить</span>
+            </a>
+        }
+        {
+          options.email.show && 
+            <a href={ 'mailto:' + options.email.link } className="option">
+              <img src="./src/assets/bobile-write.svg" />
+              <span>Написать</span>
+            </a>
+        }
+        {
+          options.whatsapp.show &&
+            <a href={ options.whatsapp.link } className="option">
+              <img src="./src/assets/bobile-whatsapp.svg" />
+              <span>WhatsApp</span>
+            </a>
+        }
+        {
+          options.telegram.show &&
+            <a  href={ options.telegram.link } className="option">
+              <img src="./src/assets/bobile-telega.svg" />
+              <span>Написать</span>
+            </a>
+        }
         <a href="#" className="leave-query mobile">Оставить заявку</a>
       </div>
     </div>
